43303ea48aa1cf635c70d483acf8406db141b536
Atsushi <gh_a2shi@afternooncafe.jp>
2013-01-21 18:00:52 +0900

    new notification preferences

---
+++ sources/NotificationController.h
@@ -0,0 +1,13 @@
+//
+//  NotificationController.h
+//  ircat
+//
+//  Created by Atsushi on 2013/01/21.
+//  Copyright (c) 2013年 atsushi. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface NotificationController : NSArrayController
+
+@end
---
+++ sources/NotificationController.m
@@ -0,0 +1,13 @@
+//
+//  NotificationController.m
+//  ircat
+//
+//  Created by Atsushi on 2013/01/21.
+//  Copyright (c) 2013年 atsushi. All rights reserved.
+//
+
+#import "NotificationController.h"
+
+@implementation NotificationController
+
+@end
